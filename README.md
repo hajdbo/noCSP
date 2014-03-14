042a6c0b08f6c4b149298a2f3bee811c921dee20
Roman Shafigullin <roman.shafigullin@gmail.com>
2014-03-13 20:37:35 -0700

    x

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ noCSP
 =====
 
 Chrome extension which disables XSS prevention and reporting
+
+Test URL
+http://xss-shafigullin-pro.appspot.com/reflector?protection=1&csp=default-src%20*&content=%3Cscript%3Ealert(1)%3C/script%3E
--- noCSP/noCSP.js
+++ noCSP/noCSP.js
@@ -1,14 +1,13 @@
 chrome.webRequest.onHeadersReceived.addListener(function(details) {
 	if (details.responseHeaders) {
-		var newHeaders = [];
+		var newHeaders = [{
+			name: 'X-XSS-Protection',
+			value: '0'
+		}];
 		details.responseHeaders.forEach(function(header) {
 			if (!/^(?:(?:X-)?Content-Security-Policy|X-WebKit-CSP)/i.test(header.name) && !/^X-XSS-Protection$/i.test(header.name)) {
 				newHeaders.push(header);
 			}
-		});
-		newHeaders.push({
-			name: 'X-XSS-Protection',
-			value: '0'
 		});
 		return {responseHeaders: newHeaders};
 	}
